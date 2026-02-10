4d51d2658d362209f191e067b8a5b41785aa5e03
Charles Nwofor <charlogazzo@gmail.com>
2026-02-10 13:36:23 +0100

    usage of etags

--- s3/iac/terraform/main.tf
+++ s3/iac/terraform/main.tf
@@ -9,6 +9,20 @@ terraform {
   }
 }
 
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = resource.aws_s3_bucket.default.id
+  key    = "file.txt"
+  source = "my_file.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("my_file.txt")
+}
+
 provider "aws" {
   # Configuration options
 }
